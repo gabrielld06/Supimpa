f6057105733fe4d02b838a00dad04ae5b12fda3b
gabrielld06 <64218838+gabrielld06@users.noreply.github.com>
2021-02-26 23:21:43 -0300

    Yocalc

---
+++ Yocalc/Yo/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace AgoraVai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.buttonMC = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.buttonX = new System.Windows.Forms.Button();
+            this.buttonSub = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonEqual = new System.Windows.Forms.Button();
+            this.buttonSom = new System.Windows.Forms.Button();
+            this.yodaPic = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.yodaPic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(68, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.AutoSize = true;
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtResult.Location = new System.Drawing.Point(50, 25);
+            this.txtResult.MinimumSize = new System.Drawing.Size(375, 50);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(375, 50);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(124, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.Location = new System.Drawing.Point(180, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 35);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.Location = new System.Drawing.Point(68, 153);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 35);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(124, 153);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 35);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button6.Location = new System.Drawing.Point(180, 153);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 35);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.Location = new System.Drawing.Point(68, 112);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 35);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button8.Location = new System.Drawing.Point(124, 112);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 35);
+            this.button8.TabIndex = 8;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button9.Location = new System.Drawing.Point(180, 112);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 35);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // buttonMC
+            // 
+            this.buttonMC.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonMC.Location = new System.Drawing.Point(248, 99);
+            this.buttonMC.Name = "buttonMC";
+            this.buttonMC.Size = new System.Drawing.Size(50, 35);
+            this.buttonMC.TabIndex = 12;
+            this.buttonMC.Text = "<-";
+            this.buttonMC.UseVisualStyleBackColor = true;
+            this.buttonMC.Click += new System.EventHandler(this.buttonMC_Click);
+            // 
+            // buttonDiv
+            // 
+            this.buttonDiv.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonDiv.Location = new System.Drawing.Point(248, 140);
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.Size = new System.Drawing.Size(50, 35);
+            this.buttonDiv.TabIndex = 11;
+            this.buttonDiv.Text = "/";
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            this.buttonDiv.Click += new System.EventHandler(this.buttonDiv_Click);
+            // 
+            // buttonX
+            // 
+            this.buttonX.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonX.Location = new System.Drawing.Point(248, 181);
+            this.buttonX.Name = "buttonX";
+            this.buttonX.Size = new System.Drawing.Size(50, 35);
+            this.buttonX.TabIndex = 10;
+            this.buttonX.Text = "x";
+            this.buttonX.UseVisualStyleBackColor = true;
+            this.buttonX.Click += new System.EventHandler(this.buttonX_Click);
+            // 
+            // buttonSub
+            // 
+            this.buttonSub.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSub.Location = new System.Drawing.Point(248, 222);
+            this.buttonSub.Name = "buttonSub";
+            this.buttonSub.Size = new System.Drawing.Size(50, 35);
+            this.buttonSub.TabIndex = 15;
+            this.buttonSub.Text = "-";
+            this.buttonSub.UseVisualStyleBackColor = true;
+            this.buttonSub.Click += new System.EventHandler(this.buttonSub_Click);
+            // 
+            // button0
+            // 
+            this.button0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button0.Location = new System.Drawing.Point(68, 235);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(50, 35);
+            this.button0.TabIndex = 14;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // buttonEqual
+            // 
+            this.buttonEqual.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonEqual.Location = new System.Drawing.Point(124, 235);
+            this.buttonEqual.Name = "buttonEqual";
+            this.buttonEqual.Size = new System.Drawing.Size(106, 35);
+            this.buttonEqual.TabIndex = 13;
+            this.buttonEqual.Text = "=";
+            this.buttonEqual.UseVisualStyleBackColor = true;
+            this.buttonEqual.Click += new System.EventHandler(this.buttonEqual_Click);
+            // 
+            // buttonSom
+            // 
+            this.buttonSom.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonSom.Location = new System.Drawing.Point(248, 263);
+            this.buttonSom.Name = "buttonSom";
+            this.buttonSom.Size = new System.Drawing.Size(50, 35);
+            this.buttonSom.TabIndex = 16;
+            this.buttonSom.Text = "+";
+            this.buttonSom.UseVisualStyleBackColor = true;
+            this.buttonSom.Click += new System.EventHandler(this.buttonSom_Click);
+            // 
+            // yodaPic
+            // 
+            this.yodaPic.ErrorImage = global::Yo.Properties.Resources.yoda1;
+            this.yodaPic.Image = global::Yo.Properties.Resources.yoda0;
+            this.yodaPic.InitialImage = null;
+            this.yodaPic.Location = new System.Drawing.Point(248, 235);
+            this.yodaPic.Name = "yodaPic";
+            this.yodaPic.Size = new System.Drawing.Size(410, 335);
+            this.yodaPic.TabIndex = 17;
+            this.yodaPic.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(664, 576);
+            this.Controls.Add(this.buttonSom);
+            this.Controls.Add(this.buttonSub);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.buttonEqual);
+            this.Controls.Add(this.buttonMC);
+            this.Controls.Add(this.buttonDiv);
+            this.Controls.Add(this.buttonX);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.yodaPic);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.yodaPic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label txtResult;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button buttonMC;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Button buttonX;
+        private System.Windows.Forms.Button buttonSub;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonEqual;
+        private System.Windows.Forms.Button buttonSom;
+        private System.Windows.Forms.PictureBox yodaPic;
+    }
+}
+
---
+++ Yocalc/Yo/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace AgoraVai
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int num;
+        public string lastAction = null;
+        public bool hadAction = false;
+
+        private void txtResult_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void Check_igual()
+        {
+
+            SoundPlayer hiii = new SoundPlayer(@"C:\Users\Gabriel\source\repos\Yo\Yo\Resources\hiii.wav");
+            hiii.Play();
+            if (lastAction == "Igual")
+            {
+                txtResult.Text = "";
+                lastAction = null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "9";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            Check_igual();
+            txtResult.Text += "0";
+        }
+
+        int Checkaction(int x)
+        {
+            if (lastAction == "Soma")
+            {
+                return num + x;
+            }
+            else if (lastAction == "Subt")
+            {
+                return num - x;
+            }
+            else if (lastAction == "Mult")
+            {
+                return num * x;
+            }
+            else if (lastAction == "Div")
+            {
+                if (x != 0)
+                {
+                    return num / x;
+                }
+                else
+                {
+                    SoundPlayer lag = new SoundPlayer(@"C:\Users\Gabriel\source\repos\Yo\Yo\Resources\lag.wav");
+                    lag.Play();
+                    MessageBox.Show("Erro: Divisão por 0");
+                    return num;
+                }
+            }
+            return num;
+        }
+
+        public void Padrao_botao()
+        {
+            if (txtResult.Text.Length != 0)
+            {
+                if (hadAction == false)
+                {
+                    num += int.Parse(txtResult.Text);
+                    txtResult.Text = "";
+                }
+                else
+                {
+                    num = Checkaction(int.Parse(txtResult.Text));
+                    txtResult.Text = "";
+                }
+                hadAction = true;
+            }
+        }
+
+        private void buttonMC_Click(object sender, EventArgs e)
+        {
+            SoundPlayer solado = new SoundPlayer(@"C:\Users\Gabriel\source\repos\Yo\Yo\Resources\solado.wav");
+            solado.Play();
+            if (txtResult.Text.Length != 0)
+            {
+                txtResult.Text = txtResult.Text.Remove(txtResult.Text.Length - 1, 1);
+            }
+        }
+
+        private void buttonSom_Click(object sender, EventArgs e)
+        {
+            SoundPlayer vamo = new SoundPlayer(@"C:\Users\Gabriel\source\repos\Yo\Yo\Resources\vamo.wav");
+            vamo.Play();
+            Padrao_botao();
+            lastAction = "Soma";
+        }
+
+        private void buttonSub_Click(object sender, EventArgs e)
+        {
+            SoundPlayer feed = new SoundPlayer(@"C:\Users\Gabriel\source\repos\Yo\Yo\Resources\feed.wav");
+            feed.Play();
+            Padrao_botao();
+            lastAction = "Subt";
+        }
+
+        private void buttonX_Click(object sender, EventArgs e)
+        {
+            SoundPlayer fon = new SoundPlayer(@"C:\Users\Gabriel\source\repos\Yo\Yo\Resources\fon.wav");
+            fon.Play();
+            Padrao_botao();
+            lastAction = "Mult";
+        }
+
+        private void buttonDiv_Click(object sender, EventArgs e)
+        {
+            SoundPlayer circo = new SoundPlayer(@"C:\Users\Gabriel\source\repos\Yo\Yo\Resources\circo.wav");
+            circo.Play();
+            Padrao_botao();
+            lastAction = "Div";
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            SoundPlayer eoq = new SoundPlayer(@"C:\Users\Gabriel\source\repos\Yo\Yo\Resources\eoq.wav");
+            eoq.Play();
+            if (txtResult.Text.Length != 0)
+            {
+                num = Checkaction(int.Parse(txtResult.Text));
+                txtResult.Text = num.ToString();
+                num = 0;
+                lastAction = "Igual";
+                hadAction = false;
+            }
+        }
+    }
+}
